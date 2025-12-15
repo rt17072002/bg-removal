a9a1c48e185ad6136d69e2578c8f37caafbf45bc
Rajeev Tripathi <rt17072002@gmail.com>
2025-12-15 15:23:10 +0530

    controller and routes added

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import "dotenv/config"
 import express from "express"
 import cors from "cors"
 import connectDB from "./config/mongodb.js";
+import userRouter from "./routes/userRoutes.js";
 
 //App config
 const port = process.env.PORT || 4000;
@@ -14,5 +15,6 @@ app.use(cors())
 
 //API routes
 app.get("/", (req,res)=>res.send("Api working"))
+app.use("/api/user", userRouter);
 
 app.listen(port, ()=>console.log(`Server is running on http://localhost:${port}`));
